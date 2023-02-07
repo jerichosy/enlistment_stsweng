009ed65747d02bebd1ff1dffdde38fc46f9cc619
andres-dalisay <andres_dalisay@dlsu.edu.ph>
2023-02-07 17:04:41 +0800

    Added test for same subject conflicts

--- src/test/java/com/orangeandbronze/enlistment/StudentTest.java
+++ src/test/java/com/orangeandbronze/enlistment/StudentTest.java
@@ -12,12 +12,14 @@ public class StudentTest {
 
     static final Schedule DEFAULT_SCHEDULE = new Schedule(Days.MTH, Period.H1000);
     static final Room DEFAULT_ROOM = new Room("AG1710", 1);
+    static final Subject DEFAULT_SUBJECT1 = new Subject("CCPROG1");
+    static final Subject DEFAULT_SUBJECT2 = new Subject("CCICOMP");
     @Test
     void enlist_2_sections_no_conflict(){
         //Given 1 student and sections w/conflict
         Student student = new Student(1);
-        Section sec1 = new Section("A", DEFAULT_SCHEDULE, DEFAULT_ROOM);
-        Section sec2 = new Section("B",  new Schedule(Days.MTH, Period.H0830), DEFAULT_ROOM);
+        Section sec1 = new Section("A", DEFAULT_SCHEDULE, DEFAULT_ROOM, DEFAULT_SUBJECT1);
+        Section sec2 = new Section("B",  new Schedule(Days.MTH, Period.H0830), DEFAULT_ROOM, DEFAULT_SUBJECT2);
         // When student enlist in both sections
         student.enlist(sec1);
         student.enlist(sec2);
@@ -34,8 +36,8 @@ public class StudentTest {
     void enlist_2_section_same_schedule(){
         //Given a student and 2 sections w/ same schedule
         Student student = new Student(1);
-        Section sec1 = new Section("A", DEFAULT_SCHEDULE, DEFAULT_ROOM);
-        Section sec2 = new Section("B", DEFAULT_SCHEDULE, DEFAULT_ROOM);
+        Section sec1 = new Section("A", DEFAULT_SCHEDULE, DEFAULT_ROOM, DEFAULT_SUBJECT1);
+        Section sec2 = new Section("B", DEFAULT_SCHEDULE, DEFAULT_ROOM, DEFAULT_SUBJECT2);
         //When student enlist in both sections
         student.enlist(sec1);
         //Then on the 2nd enlistments on exception should be thrown
@@ -43,10 +45,23 @@ public class StudentTest {
     }
 
     @Test
+    void enlist_2_section_same_subject(){
+        //Given a student and 2 sections w/ same subject
+        Schedule schedule = new Schedule(Days.MTH, H1600);
+        Student student = new Student(1);
+        Section sec1 = new Section("A", DEFAULT_SCHEDULE, DEFAULT_ROOM, DEFAULT_SUBJECT1);
+        Section sec2 = new Section("B", schedule, DEFAULT_ROOM, DEFAULT_SUBJECT1);
+        //When student enlist in both sections
+        student.enlist(sec1);
+        //Then on the 2nd enlistments on exception should be thrown
+        assertThrows(SubjectConflictException.class, ()-> student.enlist(sec2));
+    }
+
+    @Test
     void student_enlist_to_full_section(){
         Student student1 = new Student(1);
         Student student2 = new Student(2);
-        Section sec1 = new Section("A", DEFAULT_SCHEDULE, DEFAULT_ROOM);
+        Section sec1 = new Section("A", DEFAULT_SCHEDULE, DEFAULT_ROOM, DEFAULT_SUBJECT1);
         student1.enlist(sec1);
         assertThrows(Exception.class, ()-> student2.enlist(sec1));
     }
@@ -55,7 +70,7 @@ public class StudentTest {
     void student_enlist_to_open_section(){
         Student student1 = new Student(1);
         Room room = new Room("AG1710", 5);
-        Section sec1 = new Section("A", DEFAULT_SCHEDULE, room);
+        Section sec1 = new Section("A", DEFAULT_SCHEDULE, room, DEFAULT_SUBJECT1);
         student1.enlist(sec1);
         assertTrue(student1.getSections().containsAll(List.of(sec1)));
     }
@@ -64,7 +79,7 @@ public class StudentTest {
     void student_cancel_enlisted_section(){
         Student student1 = new Student(1);
         Room room = new Room("AG1710", 5);
-        Section sec1 = new Section("A", DEFAULT_SCHEDULE, room);
+        Section sec1 = new Section("A", DEFAULT_SCHEDULE, room, DEFAULT_SUBJECT1);
         student1.enlist(sec1);
         assertTrue(student1.getSections().containsAll(List.of(sec1)));
         student1.cancelEnlist(sec1);
@@ -75,7 +90,7 @@ public class StudentTest {
     void student_cancel_unenlisted_section(){
         Student student1 = new Student(1);
         Room room = new Room("AG1710", 5);
-        Section sec1 = new Section("A", DEFAULT_SCHEDULE, room);
+        Section sec1 = new Section("A", DEFAULT_SCHEDULE, room, DEFAULT_SUBJECT1);
         assertThrows(Exception.class, ()-> student1.cancelEnlist(sec1));
     }
 
