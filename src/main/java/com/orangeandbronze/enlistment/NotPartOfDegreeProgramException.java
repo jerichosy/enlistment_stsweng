5b6948d1d3faf7e446091add3e07f8f272132545
andres-dalisay <andres_dalisay@dlsu.edu.ph>
2023-02-15 21:50:44 +0800

    Added NotPartOfDegreeProgramException

---
+++ src/main/java/com/orangeandbronze/enlistment/NotPartOfDegreeProgramException.java
@@ -0,0 +1,7 @@
+package com.orangeandbronze.enlistment;
+
+public class NotPartOfDegreeProgramException extends RuntimeException{
+    public NotPartOfDegreeProgramException(String msg) {
+        super(msg);
+    }
+}
