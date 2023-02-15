fdfd9c61f1a972b1384f1139e9e1fdf226300287
andres-dalisay <andres_dalisay@dlsu.edu.ph>
2023-02-15 22:23:55 +0800

    Added InvalidPeriodException

---
+++ src/main/java/com/orangeandbronze/enlistment/InvalidPeriodException.java
@@ -0,0 +1,7 @@
+package com.orangeandbronze.enlistment;
+
+public class InvalidPeriodException extends RuntimeException{
+    public InvalidPeriodException(String msg) {
+        super(msg);
+    }
+}
