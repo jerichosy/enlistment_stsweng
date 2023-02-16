64b8203ef43f87cd155fa00226993bbfb1d9be44
FrancisTamayo <francis_tamayo@dlsu.edu.ph>
2023-02-16 21:09:18 +0800

    Created SameRoomOverlapException for Section checking room

---
+++ src/main/java/com/orangeandbronze/enlistment/SameRoomOverlapException.java
@@ -0,0 +1,7 @@
+package com.orangeandbronze.enlistment;
+
+public class SameRoomOverlapException extends RuntimeException {
+    public SameRoomOverlapException(String msg) {
+        super(msg);
+    }
+}
